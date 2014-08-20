d54880fe4cdbc7583d8dabe53b80c322e3faccfd
zeroblade1984 <zeroblade1984@gmail.com>
2014-08-20 22:30:40 +0800

    cpuidle:Small code optimization
    Signed-off-by: zeroblade1984 <zeroblade1984@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -323,12 +323,11 @@ EXPORT_SYMBOL_GPL(cpuidle_disable_device);
 static int __cpuidle_register_device(struct cpuidle_device *dev)
 {
 	int ret;
-	struct device *cpu_dev;
+	struct device *cpu_dev = get_cpu_device((unsigned long)dev->cpu);
 	struct cpuidle_driver *cpuidle_driver = cpuidle_get_driver();
 
 	if (!dev)
 		return -EINVAL;
-	cpu_dev = get_cpu_device((unsigned long)dev->cpu);
 
 	if (!try_module_get(cpuidle_driver->owner))
 		return -EINVAL;
