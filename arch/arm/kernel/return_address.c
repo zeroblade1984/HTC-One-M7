40c8fa5eab2e728a22cde00426d04a68a2a472d1
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2014-09-24 08:51:57 +0100

    ARM: 8160/1: drop warning about return_address not using unwind tables
    The warning was introduced in 2009 (commit 4bf1fa5a34aa ([ARM] 5613/1:
    implement CALLER_ADDRESSx)). The only "problem" here is that
    CALLER_ADDRESSx for x > 1 returns NULL which doesn't do much harm.
    
    The drawback of implementing a fix (i.e. use unwind tables to implement CALLER_ADDRESSx) is that much of the unwinder code would need to be marked as not
    traceable.
    
    Change-Id: I1833481898c3403aa2faafa8c36a819774aa3eee
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -55,13 +55,6 @@ void *return_address(unsigned int level)
 	else
 		return NULL;
 }
-
-#else 
-
-#if defined(CONFIG_ARM_UNWIND)
-#warning "TODO: return_address should use unwind tables"
-#endif
-
 #endif 
 
 EXPORT_SYMBOL_GPL(return_address);
