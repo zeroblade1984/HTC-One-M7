338bda289a6c5081436866732b6e147098145202
zeroblade1984 <zeroblade1984@gmail.com>
2014-10-02 20:41:02 +0800

    msm: avs: Remove vestigial software based AVS Older targets like 8x50 had the first version of Apdaptive Voltage Scaling done in software. This feature was never enabled and supported. Remove the code and the associated config items.
    Change-Id: If3bdab7d0bdb5d51e099f7c37f56592826edc102
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>
    
    (Cherry picked and adapted to M7)
    Signed-off-by: zeroblade1984 <zeroblade1984@gmail.com>

--- arch/arm/mach-msm/avs.h
+++ arch/arm/mach-msm/avs.h
@@ -14,22 +14,6 @@
 #ifndef AVS_H
 #define AVS_H
 
-#define VOLTAGE_MIN  1000 
-#define VOLTAGE_MAX  1250
-#define VOLTAGE_STEP 25
-
-int __init avs_init(int (*set_vdd)(int), u32 freq_cnt, u32 freq_idx);
-void __exit avs_exit(void);
-
-int avs_adjust_freq(u32 freq_index, int begin);
-
-#ifdef CONFIG_MSM_CPU_AVS
-u32 avs_test_delays(void);
-#else
-static inline u32 avs_test_delays(void)
-{ return 0; }
-#endif
-
 #ifdef CONFIG_MSM_AVS_HW
 u32 avs_reset_delays(u32 avsdscr);
 u32 avs_get_avscsr(void);
@@ -49,8 +33,6 @@ static inline u32 avs_get_tscsr(void)
 static inline void avs_set_tscsr(u32 to_tscsr) {}
 static inline void avs_disable(void) {}
 #endif
-
-#define AVSDEBUG(...)
 
 #define AVS_DISABLE(cpu) do {			\
 		if (get_cpu() == (cpu))		\
